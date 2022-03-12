8a3c0e8c77d72908114f1719352b34f572144f49
Janujan Gathieswaran <jgathies@uwaterloo.ca>
2022-03-12 15:52:46 -0500

    Add toolbar for graphs

--- Dashboard.py
+++ Dashboard.py
@@ -7,36 +7,52 @@ import matplotlib.pyplot as plt
 
 root = Tk()
 root.title("Fridge Dashboard")
+# canvas = Canvas(root, height = 700, width = 700, bg="#263D42")
+# canvas.pack()
+isOn = False
 
 def loadCSV():
     filename = filedialog.askopenfile(initialdir="/", title="Select csv")
     print (filename)
+    isOn = not isOn
+    if (isOn):
+        fanBtn["text"] = "On"
+    else:
+        fanBtn["text"] = "Off"
 
 
 def matplotCanvas():
-    figure1 = Figure(figsize=(5,5), dpi=100)
+    figure1 = Figure(figsize=(10,5), dpi=100)
     ax1 = figure1.add_subplot(111)
     ax1.plot([1,2,3,4,5,6,7,8], [23,22,34,2,1,44,2,23])
     ax1.set_title("Average temperature")
     ax1.set_xlabel("Time (s)")
     ax1.set_ylabel("Temp (C)")
 
-    figureCanvas = FigureCanvasTkAgg(figure1, root)
+    figureCanvas1 = FigureCanvasTkAgg(figure1, root)
     # canvas.show()
-    figureCanvas.get_tk_widget().pack(side=LEFT, fill=BOTH, expand=TRUE)
+    figureCanvas1.get_tk_widget().pack(side=LEFT, fill=BOTH, expand=TRUE)
     # side=BOTTOM, fill=BOTH, expand=TRUE
 
-    figure2 = Figure(figsize=(5,5), dpi=100)
+    #toolbar for figure1
+    toolbar = NavigationToolbar2Tk(figureCanvas1, root)
+    figureCanvas1._tkcanvas.pack(side=BOTTOM, fill=BOTH, expand=TRUE)
+    #anchor=NW
+    
+    figure2 = Figure(figsize=(10,5), dpi=100)
     ax2 = figure2.add_subplot(111)
     ax2.plot([1,2,3,4,5,6,7,8], [23,22,34,2,1,44,2,23])
     ax2.set_title("Temperature Deviation")
     ax2.set_xlabel("Time (s)")
     ax2.set_ylabel("Deviation")
 
-    figureCanvas = FigureCanvasTkAgg(figure2, root)
+    figureCanvas2 = FigureCanvasTkAgg(figure2, root)
     # canvas.show()
-    figureCanvas.get_tk_widget().pack(side=LEFT, fill=BOTH, expand=TRUE)
+    figureCanvas2.get_tk_widget().pack(side=LEFT, fill=BOTH, expand=TRUE)
 
+    #toolbar for figure2
+    toolbar = NavigationToolbar2Tk(figureCanvas2, root)
+    figureCanvas2._tkcanvas.pack(side=BOTTOM, fill=BOTH, expand=TRUE)
     #add navigation bar for graph
 
 
@@ -45,10 +61,10 @@ matplotCanvas()
 # canvas = Canvas(height=700, width=1000, bg="white")
 # canvas.pack()
 
-# frame = Frame(root, bg="black")
+# frame = Frame(root, bg="grey")
 # frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-logslabel = Label(text="Import logs")
-logslabel.pack()
+# logslabel = Label(text="Import logs")
+# logslabel.pack()
 
 openFile = Button(text="Open file", padx=10, pady=5, command=loadCSV)
 openFile.pack()
