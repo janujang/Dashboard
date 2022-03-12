27379063df2551bf1e2708f15e48fc300e9e1c6d
Janujan Gathieswaran <jgathies@uwaterloo.ca>
2022-03-12 18:59:12 -0500

    Animate subplots

--- LoadDataFromFileSubPlots.py
+++ LoadDataFromFileSubPlots.py
@@ -15,6 +15,12 @@ index = count()
 
 # fig, ax = plt.subplots()
 
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# fig.show()
+
+fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
+
 def animate(i):
     #for posting temperature to an endpoint
     # r = requests.post('http://myserver.com/api', json={"temp": "28"}) 
@@ -29,14 +35,22 @@ def animate(i):
     data = pd.read_csv('data.csv')
     x = data['x_value']
     y1 = data['temp_1']
-    # y2 = data['total_2']
+    y2 = data['temp_2']
 
-    plt.cla()
+    ax1.cla()
+    ax2.cla()
+    ax1.plot(x, y1, label='Temp 1')
+    ax2.plot(x, y2, label='Temp 2')
+    ax1.set_ylim(bottom=0, top=10)
+    ax2.set_ylim(bottom=0, top=10)
+    ax1.set_xlim(left=max(0, i-10), right=i+10)
+    ax2.set_xlim(left=max(0, i-10), right=i+10)
+    # print(i)
 
-    plt.plot(x, y1, label='Channel 1')
+
     # plt.plot(x, y2, label='Channel 2')
 
-    plt.legend(loc='upper left')
+    ax1.legend(loc='upper left')
     # plt.tight_layout()
     
 
@@ -44,8 +58,9 @@ def animate(i):
 
 ani = FuncAnimation(plt.gcf(), animate, interval=1000)
 
-plt.title("Temperature Readings")
-plt.xlabel("Time (secs)")
-plt.xlabel("Temp (C)")
-plt.tight_layout()
+ax1.set_title("Temperature Readings")
+ax1.set_xlabel("Time (secs)")
+ax1.set_xlabel("Temp (C)")
 plt.show()
+plt.tight_layout()
+# fig.show()
