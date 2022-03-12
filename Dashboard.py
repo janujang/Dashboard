859af7938c3b9213e25fd64056aee41f681e77fd
Janujan Gathieswaran <jgathies@uwaterloo.ca>
2022-03-12 14:18:06 -0500

    Changes to dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -3,8 +3,10 @@ import os
 from tkinter import filedialog
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-root = Tk()
+import matplotlib.pyplot as plt
 
+root = Tk()
+root.title("Fridge Dashboard")
 
 def loadCSV():
     filename = filedialog.askopenfile(initialdir="/", title="Select csv")
@@ -12,26 +14,43 @@ def loadCSV():
 
 
 def matplotCanvas():
-    f = Figure(figsize=(5,5), dpi=100)
-    a = f.add_subplot(111)
-    a.plot([1,2,3,4,5,6,7,8], [23,22,34,2,1,44,2,23])
+    figure1 = Figure(figsize=(5,5), dpi=100)
+    ax1 = figure1.add_subplot(111)
+    ax1.plot([1,2,3,4,5,6,7,8], [23,22,34,2,1,44,2,23])
+    ax1.set_title("Average temperature")
+    ax1.set_xlabel("Time (s)")
+    ax1.set_ylabel("Temp (C)")
 
-    canvas = FigureCanvasTkAgg(f)
-    canvas.show()
-    canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=TRUE)
+    figureCanvas = FigureCanvasTkAgg(figure1, root)
+    # canvas.show()
+    figureCanvas.get_tk_widget().pack(side=LEFT, fill=BOTH)
+    # side=BOTTOM, fill=BOTH, expand=TRUE
+
+    figure2 = Figure(figsize=(5,5), dpi=100)
+    ax2 = figure2.add_subplot(111)
+    ax2.plot([1,2,3,4,5,6,7,8], [23,22,34,2,1,44,2,23])
+    ax2.set_title("Temperature Deviation")
+    ax2.set_xlabel("Time (s)")
+    ax2.set_ylabel("Deviation")
+
+    figureCanvas = FigureCanvasTkAgg(figure2, root)
+    # canvas.show()
+    figureCanvas.get_tk_widget().pack(side=LEFT, fill=BOTH)
+
 
 
 matplotCanvas()
-canvas = Canvas(root, height=700, width=1000, bg="white")
-canvas.pack()
+# canvas = Canvas(height=700, width=1000, bg="white")
+# canvas.pack()
 
-frame = Frame(root, bg="black")
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-label = Label(root, text="Hello World!")
+# frame = Frame(root, bg="black")
+# frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+label = Label(text="Actions")
+label.pack()
 
-openFile = Button(root, text="Open log", padx=10, pady=5, command=loadCSV)
-
+openFile = Button(text="Open log", padx=10, pady=5, command=loadCSV)
 openFile.pack()
+
 
 
 root.mainloop()
