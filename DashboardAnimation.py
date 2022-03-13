6d7d3179d9df0fd4a4e9145916c689c8e643e7bb
Janujan Gathieswaran <jgathies@uwaterloo.ca>
2022-03-13 17:05:57 -0400

    Use reeve's code in dashboard

--- DashboardAnimation.py
+++ DashboardAnimation.py
@@ -5,9 +5,11 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import ticklabel_format
 import pandas as pd
 import csv
 from matplotlib import style
+import math
 
 root = Tk()
 root.title("Fridge Dashboard")
@@ -38,83 +40,82 @@ def loadCSV():
 fig = Figure(figsize=(10,10), dpi=100)
 ax1 = fig.add_subplot(211)
 ax2 = fig.add_subplot(212)
-data, x, temp1, temp2, fan, compressor = [], [], [], [], [], []
 
 def animate(i):
     #for posting temperature to an endpoint
     # r = requests.post('http://myserver.com/api', json={"temp": "28"}) 
     # print(r.status_code)
 
-
     #get temperature
     # r = requests.get("http://api.plos.org/search?q=title:DNA")
     # print(r.content['journal'])
     # temp = r.json()
 
-    # use this approach to show information about the statuses and alerts
-    with open('data.csv', 'r') as file:
-        # [line.strip().split(',')[-1] for line in file.readlines()]
-        data = file.readlines()[-1].split(',')
-        # print(data)
-        # reader = csv.reader(file)
-        # # for row in reader:
-        # #     print (row)
-        # print(reader[-1])
-
-    # use this approach to show graphs
     dataCsv = pd.read_csv('data.csv')
-    x = dataCsv['x_value']
-    temp1Csv = dataCsv['temp_1']
-    temp2Csv = dataCsv['temp_2']
-    temp3 = dataCsv['temp_3']
-    temp4 = dataCsv['temp_4']
-    temp5 = dataCsv['temp_5']
-    # fan = dataCsv['fan']
-    # compressor = dataCsv['compressor']
 
-    # x.append(data[0])
-    temp1.append(data[1])
-    temp2.append(data[2])
-    # temp3 = data[3]
-    # temp4 = data[4]
-    # temp5 = data[5]
-    fan.append(data[6])
-    compressor.append(data[7])
+    deviations = []
+    avgTemp = []
+    fanStates = []
+    compressorStates = []
+
+    x = dataCsv.x_value
+    t1 = dataCsv.temp_1
+    t2 = dataCsv.temp_2
+    t3 = dataCsv.temp_3
+    t4 = dataCsv.temp_4
+    t5 = dataCsv.temp_5
+    fan = dataCsv.fan
+    compressor = dataCsv.compressor
+
+    for i in range(0, len(t1)):
+        avg = 0.2*(t1[i] + t2[i] + t3[i] + t4[i] + t5[i])
+        avgTemp.append(avg)
+        rms = math.sqrt((0.2)*(t1[i]*t1[i] + t2[i]*t2[i] +t3[i]*t3[i] + t4[i]*t4[i] + t5[i]*t5[i])) - avgTemp[i]
+        deviations.append(rms)
+        fanStates.append(fan[i])
+        compressorStates.append(fan[i])
 
     ax1.clear()
     ax2.clear()
-    ax1.plot(x, temp1Csv, label='Temp 1')
-    ax2.plot(x, temp2Csv, label='Temp 2')
+
+    ax1.plot(x, avgTemp, label='Average Temperature')
+    ax1.plot(x, deviations, label='Temperature Deviation')
+    ax2.plot(x, fan, label='Fan State (on/off)')
+    ax2.plot(x, compressor, label='Compressor State (on/off)')
+    
     ax1.set_ylim(bottom=0, top=10)
-    ax2.set_ylim(bottom=0, top=10)
-    # ax1.set_xlim(left=max(0, i-10), right=i+10)
-    # ax2.set_xlim(left=max(0, i-10), right=i+10)
+    ax2.set_ylim(bottom=0, top=1)
+    ax1.set_xlim(left=max(0, i-10), right=i+10)
+    ax2.set_xlim(left=max(0, i-10), right=i+10)
     # print(i)
-
-
-    # plt.plot(x, y2, label='Channel 2')
 
     ax1.legend(loc='upper left')
     ax2.legend(loc='upper left')
+
     # plt.tight_layout()
-    ax1.set_title("Temperature Readings (1)")
-    ax1.set_xlabel("Time (secs)")
-    ax1.set_ylabel("Temp (C)")
+    ax1.set_title("Temperature Data")
+    ax1.set_xlabel("Time (s)")
+    ax1.set_ylabel("Temp (°C)")
 
-    ax2.set_title("Temperature Readings (2)")
-    ax2.set_xlabel("Time (secs)")
-    ax2.set_ylabel("Temp (C)")
+    ax2.set_title("Actuator States")
+    ax2.set_xlabel("Time (s)")
+    ax2.set_ylabel("On/Off")
+    # ax2.set_yticklabels(ticklabel_format.astype(int))
 
-    if (fan[-1] == '1'):
+    if (fanStates[-1] == 1):
         fanBtn["text"] = "On"
     else:
         fanBtn["text"] = "Off"
 
-    # print (int(temp1[-1]))
-    if (int(temp1[-1]) >= 8):
-        alertLabel["text"] = "Warning"
+    if (compressorStates[-1] == 1):
+        compressorBtn["text"] = "On"
     else:
-        alertLabel["text"] = "Good"
+        compressorBtn["text"] = "Off"
+
+    if (avgTemp[-1] >= 6 or deviations[-1] >= 2):
+        alertLabel["text"] = "Outside of range"
+    else:
+        alertLabel["text"] = "Within range"
 
 
 
